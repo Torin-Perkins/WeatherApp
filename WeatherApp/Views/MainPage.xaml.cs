218951640d38643e7424c951484ffeb46d438f94
Torin-Perkins <tdmp42@gmail.com>
2024-12-14 11:02:16 -0800

    Add project files.

---
+++ WeatherApp/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void CityButton_Clicked(object sender, EventArgs e)
+        {
+            return;
+        }
+    }
+
+}
